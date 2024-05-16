eb33cf0768024c5ee45391164f15dfc22eeb90c1
romiyadangol <dangolromiya@gmail.com>
2024-05-16 11:36:37 +0545

    Updated

--- random-post/src/App.jsx
+++ random-post/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import "./assets/card.css";
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -6,14 +6,24 @@ function App() {
   const [userId, setUserId] = useState(1);
   const [filteredPosts, setFilteredPosts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
+      .then(response => {
+        if(response.ok) {
+          return response.json();
+      }
+      throw response;
+      })
       .then(data => {
         setPosts(data);
         setFilteredPosts(data.filter(post => post.userId === userId));
+        setLoading(false); 
+      })
+      .catch(error => {
+        setError(error);
         setLoading(false); 
       });
   }, []);
@@ -35,16 +45,26 @@ function App() {
 
   return (
     <>
+    <div className="header">
+        <h1>User ID: {userId}</h1>
+    </div>
       <div className="card-wrapper">
-        {loading ? (
+      {error ? (
+          <p>Error: {error}</p>
+        ) :
+        loading ? (
           <p>Loading...</p>
         ) : (
           filteredPosts.map((value, index) => (
             <div key={index} className='container'>
-              <p>userID: {value.userId}</p>
-              <p>id: {value.id}</p>
-              <p>title: {value.title}</p>
-              <p>body: {value.body}</p>
+              <div className="content">
+                <span>id: {value.id}</span>
+                <h2>{value.title}</h2>
+                <p>{value.body}</p>
+              </div>
+              <div className="image">
+                <img src={`https://picsum.photos/200/300?random=${index}`} alt="" />
+              </div>
             </div>
           ))
         )}
